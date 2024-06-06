ec650051552042768b109c8228d2756304dd2069
Batyr Atayev <atayev2012@gmail.com>
2024-06-06 20:58:13 +0300

    Added schemas for trip create function

---
+++ travels/src/travel_create_app/schemas.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+
+
+class CreateNewTrip(BaseModel):
+    driver_id: int = Field(examples=["45367"])
+    routes: list = Field(examples=[["Moscow", "Vladimir", "Nizhniy Novgorod"]])
+    departure_date: datetime = Field(examples=["2024-06-05T23:39:47"])
+    car_id: int = Field(examples=["567"])
+    passenger_capacity: int = Field(examples=["3"])
+    travel_cost: int = Field(examples=["30000"]) # cost 300.00 is written like 30000
