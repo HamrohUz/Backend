4af12085025c91f3df1b0080c77b0d8a2adacb92
Batyr Atayev <atayev2012@gmail.com>
2024-06-07 01:53:31 +0300

    Added GET Endpoint - user_trips_info and POST Endpoint find_matching_trips

--- travels/src/travel_create_app/router.py
+++ travels/src/travel_create_app/router.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 
 from travels.src.database import get_async_session
-from travels.src.models import travels, travel_progress, travel_routes
-from travels.src.passenger_application_app.schemas import PassengerApplyForTravel
+from travels.src.models import travels, travel_progress, travel_routes, passengers
 
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import insert
+from sqlalchemy import insert, select, extract
 
 from travels.src.travel_create_app.utils import trip_create_error_check
-from travels.src.travel_create_app.schemas import CreateNewTrip
-from travels.src.travel_create_app.utils import internal_server_error
+from travels.src.travel_create_app.schemas import CreateNewTrip, GetTripInfo, FindTrip
+from travels.src.travel_create_app.utils import internal_server_error, trip_status_translator
+from datetime import datetime
 
 router = APIRouter(
     prefix="/api/trip",
@@ -30,7 +30,7 @@ async def trip_create(trip_data: CreateNewTrip, session: AsyncSession = Depends(
         await session.commit()
 
         statement_travel_progress_add = insert(travel_progress).values(
-            travel_id=travel_id, progress_status=trip_data["travel_status"]
+            travel_id=travel_id, progress_status="Not_started"
         )
 
         await session.execute(statement_travel_progress_add)
@@ -50,8 +50,117 @@ async def trip_create(trip_data: CreateNewTrip, session: AsyncSession = Depends(
         "status": "OK",
         "success": True,
         "message": f"Trip was created successfully!",
-        "data": None
+        "data": {
+            "trip_id": travel_id
+        }
     }
+
+
+@router.get("/user-trips-info")
+async def user_trips_info(user_id: int,  session: AsyncSession = Depends(get_async_session)):
+    try:
+        query_as_passenger = select(passengers.c.travel_id).where(passengers.c.passenger_id == user_id)
+        query_as_passenger_result = await session.execute(query_as_passenger)
+        query_as_passenger_data = query_as_passenger_result.fetchall()
+
+        data_as_passenger = tuple([i[0] for i in query_as_passenger_data])
+
+        query_get_travels = select(travels).where((travels.c.id.in_(data_as_passenger)) | (travels.c.driver_id == user_id))
+        query_get_travels_result = await session.execute(query_get_travels)
+        query_get_travels_data = query_get_travels_result.fetchall()
+
+        result_data_dict = {"trips": []}
+        for travel in query_get_travels_data:
+            result_data_dict["trips"].append(
+                {
+                    "trip_driver_id": travel[1],
+                    "trip_id": travel[0],
+                    "trip_origin": travel[2],
+                    "trip_destination": travel[3],
+                    "trip_date": travel[4],
+                    "trip_car_id": travel[5],
+                    "trip_status": trip_status_translator[travel[6]],
+                    "trip_passenger_qty": travel[8],
+                    "trip_cost": travel[9]
+                }
+            )
+
+    except Exception as e:
+        await internal_server_error(e)
+
+    if len(result_data_dict["trips"]) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "satusCode": status.HTTP_404_NOT_FOUND,
+                "status": "NOT_FOUND",
+                "success": False,
+                "message": "У вас нет никаких поездок!",
+                "data": None
+            }
+        )
+
+    return {
+        "satusCode": status.HTTP_200_OK,
+        "status": "OK",
+        "success": True,
+        "message": f"Data was exported successfully!",
+        "data": result_data_dict
+    }
+
+@router.post("/search")
+async def find_matching_trips(lookup_trip_data: FindTrip, session: AsyncSession = Depends(get_async_session)):
+    trip_data = lookup_trip_data.model_dump()
+    try:
+        query = select(travels).where(
+            (travels.c.travel_origin == trip_data["origin"]) &
+            (travels.c.travel_destination == trip_data["destination"]) &
+            (extract("year", travels.c.departure_date) == trip_data["date_time"].year) &
+            (extract("month", travels.c.departure_date) == trip_data["date_time"].month) &
+            (extract("day", travels.c.departure_date) == trip_data["date_time"].day) &
+            ((travels.c.passenger_capacity - travels.c.passenger_actual) > 0)
+        )
+
+        query_result = await session.execute(query)
+        query_data = query_result.fetchall()
+
+        found_trips = {
+            "trips": []
+        }
+
+        for trip in query_data:
+            found_trips["trips"].append(
+                {
+                    "trip_driver_id": trip[1],
+                    "trip_id": trip[0],
+                    "trip_origin": trip[2],
+                    "trip_destination": trip[3],
+                    "trip_date": trip[4],
+                    "trip_car_id": trip[5],
+                    "trip_status": trip_status_translator[trip[6]],
+                    "trip_passenger_qty": trip[8],
+                    "trip_passenger_seats_left": trip[7] - trip[8],
+                    "trip_cost": trip[9]
+                }
+            )
+    except Exception as e:
+        await internal_server_error(e)
+
+    if len(found_trips["trips"]) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "satusCode": status.HTTP_404_NOT_FOUND,
+                "status": "NOT_FOUND",
+                "success": False,
+                "message": "Поездок по маршруту не найдено!",
+                "data": None
+            }
+        )
+
+    return found_trips
+
+
 
 # TODO 1: async def trip_edit()
 # TODO 2: async def trip_delete()
